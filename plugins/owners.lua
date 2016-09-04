55bfe9180fb6a62e7779aa0aaf8c06f5f12c0d93
mrjalta <mr.jalta90@gmail.com>
2016-09-05 00:33:39 +0300

    Update owners.lua

--- plugins/owners.lua
+++ plugins/owners.lua
@@ -1,7 +1,7 @@
 --[[
 # For More Information ....! 
-# Developer : Aziz < @devss_bot >      #Dev
-# our channel: @help_tele
+# Developer : Amoure < @Tawasle1_bot >      #Dev
+# our channel: @Amoure123
 ]]
 local function lock_group_namemod(msg, data, target)
   local group_name_set = data[tostring(target)]['settings']['set_name']
