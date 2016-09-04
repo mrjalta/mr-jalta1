16319464437dec46a5753733e72a176ef18a0889
mrjalta <mr.jalta90@gmail.com>
2016-09-05 00:25:54 +0300

    Update msg_checks.lua

--- plugins/msg_checks.lua
+++ plugins/msg_checks.lua
@@ -1,7 +1,7 @@
 --[[
 # For More Information ....! 
-# Developer : Aziz < @devss_bot >      #Dev
-# our channel: @help_tele
+# Developer : Amoure < @Tawasle1_bot >      #Dev
+# our channel: @Amoure123
 ]]
 --Begin msg_checks.lua
 --Begin pre_process function
