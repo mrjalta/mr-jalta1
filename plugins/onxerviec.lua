76edd80a5abe14e358c7b9fe39566f59b544072d
mrjalta <mr.jalta90@gmail.com>
2016-09-05 00:31:42 +0300

    Update onxerviec.lua

--- plugins/onxerviec.lua
+++ plugins/onxerviec.lua
@@ -1,7 +1,7 @@
 --[[
 # For More Information ....! 
-# Developer : Aziz < @devss_bot >      #Dev
-# our channel: @help_tele
+# Developer : Amoure < @Tawasle1_bot >      #Dev
+# our channel: @Amoure123
 ]]
 do
 -- Will leave the group if be added
