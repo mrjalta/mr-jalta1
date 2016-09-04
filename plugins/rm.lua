e6016c5e2d10f179ec4e02f35abc6df227642707
mrjalta <mr.jalta90@gmail.com>
2016-09-05 00:42:21 +0300

    Update rm.lua

--- plugins/rm.lua
+++ plugins/rm.lua
@@ -1,7 +1,7 @@
 --[[
 # For More Information ....! 
-# Developer : Aziz < @devss_bot >      #Dev
-# our channel: @help_tele
+# Developer : Amoure < @Tawasle1_bot >      #Dev
+# our channel: @Amoure123
 ]]
 local function history(extra, suc, result)
   for i=1, #result do
