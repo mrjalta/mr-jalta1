ff28dd58215f7c0aa9165db09ca78ef530117eb0
mrjalta <mr.jalta90@gmail.com>
2016-09-04 23:57:36 +0300

    Update image.lua

--- plugins/image.lua
+++ plugins/image.lua
@@ -1,7 +1,7 @@
 --[[
 # For More Information ....! 
-# Developer : Aziz < @devss_bot >      #Dev
-# our channel: @help_tele
+# Developer : Amoure < @Tawasle1_bot >      #Dev
+# our channel: @Amoure123
 ]]
 local function toimage(msg, success, result)
   local receiver = get_receiver(msg)
