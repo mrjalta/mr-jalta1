495276ec643c196be1edd3822e64d25042b6fec4
mrjalta <mr.jalta90@gmail.com>
2016-09-04 23:54:28 +0300

    Update get.lua

--- plugins/get.lua
+++ plugins/get.lua
@@ -1,7 +1,7 @@
 --[[
 # For More Information ....! 
-# Developer : Aziz < @devss_bot >      #Dev
-# our channel: @help_tele
+# Developer : Amoure < @Tawasle1_bot >      #Dev
+# our channel: @Amoure123
 ]]
 local function get_variables_hash(msg)
   if msg.to.type == 'chat' or msg.to.type == 'channel' then
