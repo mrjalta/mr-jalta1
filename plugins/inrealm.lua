e041c5b52a2c5d40e599aa030e4961d6c65af06e
mrjalta <mr.jalta90@gmail.com>
2016-09-05 00:02:00 +0300

    Update inrealm.lua

--- plugins/inrealm.lua
+++ plugins/inrealm.lua
@@ -1,7 +1,7 @@
 --[[
 # For More Information ....! 
-# Developer : Aziz < @devss_bot >      #Dev
-# our channel: @help_tele
+# Developer : Amoure < @Tawasle1_bot >      #Dev
+# our channel: @Amoure123
 ]]
 -- data saved to moderation.json
 -- check moderation plugin
